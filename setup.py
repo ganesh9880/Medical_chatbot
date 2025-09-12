bcd315317fecf22e613c2899bfad67d462fd376a
ganesh9880 <ganesh.bonu16@gmail.com>
2025-09-12 16:31:45 +0530

    Project Structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Bonu Sai Ganesh',
+    author_email='ganesh.bonu16@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
